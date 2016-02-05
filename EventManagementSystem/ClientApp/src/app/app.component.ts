6de7d472c1a8475044c8e94336b4bb328612eaf3
helios0722 <90462652+helios0722@users.noreply.github.com>
2016-02-05 10:50:00 -0800

    Bug Fixed - D:/Work/@/event-system-exercise/EventManagementSystem/ClientApp/src/app/app.component.ts

--- EventManagementSystem/ClientApp/src/app/app.component.ts
+++ EventManagementSystem/ClientApp/src/app/app.component.ts
@@ -1,10 +1,1 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass'],
-})
-export class AppComponent {
-  title = 'EventManagementSystem';
-}
+"Fri Feb 05 2016 10:50:00 GMT-0800 (Pacific Standard Time)" 
